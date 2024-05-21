d3bd9868fc1d78edbea6c795430238ad83be3c8e
Gurpreet-05 <142571616+Gurpreet-05@users.noreply.github.com>
2024-05-21 16:26:37 +0530

    Add files via upload

---
+++ Evenodd.js
@@ -0,0 +1,7 @@
+let play = document.querySelector(".button4");
+let conditions = document.querySelector(".conditions");
+let main = document.querySelector(".main");
+play.addEventListener("click", ()=>{
+    main.style.opacity="1";
+    conditions.style.opacity="0";
+})
